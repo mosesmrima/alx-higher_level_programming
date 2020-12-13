3e4c5114644b6f20e504d0129a434bf0498239ed
mosesmrima <mrimamss@gmail.com>
2020-12-13 23:36:53 +0300

    check cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ *check_cycle - checks for a cycle in a linked list
+ *@list : head of the linked list
+ *Return - 1 if cycle present, 0 if cycle absent
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *first;
+	listint_t *other;
+
+	other = first = list;
+
+	while (1)
+	{
+		first = first->next;
+
+		if (other->next != NULL)
+			other = other->next->next;
+
+		else
+			return (0);
+		if (first == NULL || other == NULL)
+			return (0);
+
+		if (first == other)
+			return (1);
+	}
+}
