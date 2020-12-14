7e19609f9fc0141864e71ce6d08c7f9d74343a10
mosesmrima <mrimamss@gmail.com>
2020-12-14 21:41:35 +0300

    Char conversion

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def islower(c):
+    for i in c[:]:
+        if 97 <= ord(i) <= 122:
+            m = ord(i)
+            m = m - 32
+            i = m
+            print(chr(i), end="")
+        else:
+            print(i, end="")
