f1f1e9620faa90fbd66ef3f42aa6c5b5e941e1db
mosesmrima <mrimamss@gmail.com>
2020-12-17 13:45:12 +0300

    argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    argc = len(argv)
+    print("{} arguments".format(argc-1))
+    for i in range(1, argc):
+        print("{}: {}".format(i, argv[i]))
