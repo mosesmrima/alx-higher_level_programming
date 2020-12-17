777fc27a9f858fdee2896b6f1116362751dd6484
mosesmrima <mrimamss@gmail.com>
2020-12-17 13:48:26 +0300

    added check for 1 argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,6 +2,9 @@
 from sys import argv
 if __name__ == "__main__":
     argc = len(argv)
-    print("{} arguments".format(argc-1))
-    for i in range(1, argc):
+    if argc == 2:
+        print("{} argument".format(argc - 1))
+    else:
+        print("{} arguments".format(argc-1))
+    for i in range (1, argc):
         print("{}: {}".format(i, argv[i]))
