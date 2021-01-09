bf800799b1199930ecf93af38b7b29ae35d6840c
mosesmrima <mrimamss@gmail.com>
2021-01-10 02:06:11 +0300

    print square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+This module cntains ol ine class
+"""
+
+
+class Square:
+    """
+    A class with a private instance variable size
+    Also chcks if size an integer and greater than or equal to zero
+    """
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        for j in range(self.__size):
+            for i in range(self.__size):
+                print("#", end="")
+                if i == self.__size - 1:
+                    print("")
