5b43c89cfa046ce3bc1ce2ba5c89e0240196d147
mosesmrima <mrimamss@gmail.com>
2021-01-10 01:02:27 +0300

    added square method to class

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module cntains ol ine class
+"""
+
+
+class Square:
+    """
+    A class with a private instance variable size
+    Also chcks if size an integer and greater than or equal to zero
+    """
+
+    def __init__(self, size=0):
+        self.__size = size
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
