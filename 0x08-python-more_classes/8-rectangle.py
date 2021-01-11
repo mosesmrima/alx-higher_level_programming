b62d1b5318111e93c9e360e189ff903528ca3e5c
mosesmrima <mrimamss@gmail.com>
2021-01-11 20:58:31 +0300

    fixed typo

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -112,4 +112,4 @@ class Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_1.area() >= rect_2.area():
             return rect_1
-        return rect
+        return rect_2
