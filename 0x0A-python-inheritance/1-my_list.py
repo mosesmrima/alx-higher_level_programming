843043d0f98ca447887659cb7d90b427c8e41cc8
mosesmrima <mrimamss@gmail.com>
2021-01-25 14:21:49 +0300

    return list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+this module contains one class: Mylist
+"""
+
+
+class MyList(list):
+    """
+    class contains one method
+    """
+    def print_sorted(self):
+        print(sorted(self))
