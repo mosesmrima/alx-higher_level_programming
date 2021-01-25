83468cc75a9ee45627dac5971b1509f73073d48a
mosesmrima <mrimamss@gmail.com>
2021-01-25 14:29:40 +0300

    has one method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+module contains one empty class
+"""
+
+
+class BaseGeometry:
+    """
+    class with one method
+    """
+    def area(self):
+        """
+        method raises exception
+        :return: does not return
+        """
+        raise Exception("area() is not implemented")
