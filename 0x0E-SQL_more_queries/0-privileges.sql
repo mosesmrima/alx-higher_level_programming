8d746c571a0dd1291eb2d059cdd3494ccd5525b3
mosesmrima <mrimamss@gmail.com>
2021-03-29 17:09:11 +0300

    edit

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- This script shows the grans for 2 users
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localost';
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localost;
