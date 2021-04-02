6e4cf1e14ce4df1c0721e4246966efaf159ed5fe
mosesmrima <mrimamss@gmail.com>
2021-04-02 23:15:24 +0300

    fixed syntax error

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,7 +8,7 @@ import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM `states` WHERE BINARY `name` =  {}\
+    cur.execute("SELECT * FROM `states` WHERE BINARY `name` =  '{}'\
  ORDER BY id ASC".format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
