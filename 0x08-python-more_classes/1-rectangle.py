52a019f512bc6122cc82ce2751f71824f372964a
mosesmrima <mrimamss@gmail.com>
2021-10-04 23:47:55 +0300

    add properties to Rectangle class

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+""" Module contains the Rectangle class """
+
+
+class Rectangle:
+    """Define the Rectangle class"""
+
+    def __init__(self, width=0, height=0):
+        """Init method"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get/Set width property"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self, value):
+        """Get/Set height property"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Height setter"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
