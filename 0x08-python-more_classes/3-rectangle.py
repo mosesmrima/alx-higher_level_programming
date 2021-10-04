589f71485b4ff587f083c035b2b367252b441a8f
mosesmrima <mrimamss@gmail.com>
2021-10-05 01:03:36 +0300

    __str__

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+
+""" This module contains the rectangle class """
+
+
+class Rectangle:
+    """Rectangle class defination"""
+
+    def __init__(self, width=0, height=0):
+        """init method"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """get/set width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """set/get height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """set height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """return area"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """return perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width + self.__height) * 2
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        string = []
+        for i in range(self.__height):
+            [string.append("#") for j in range(self.__width)]
+            string.append("\n")
+        return "".join(string)
