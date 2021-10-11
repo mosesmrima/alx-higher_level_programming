6b9542352770b741bf31fd17c44346eee4c2ede5
mosesmrima <mrimamss@gmail.com>
2021-10-12 01:34:04 +0300

    use dir

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -6,7 +6,7 @@ this module contains one method
 
 def lookup(obj):
     """
-    :param obj: the object to look up its available params and methods
-    :return :a list of all available methods and attributes of an object
+    param obj: the object to look up its available params and methods
+    return :a list of all available methods and attributes of an object
     """
     return dir(obj)
