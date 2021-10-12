27782d7bcb1f3a3c1e1b4c9f3ba757af6f17ed75
mosesmrima <mrimamss@gmail.com>
2021-10-12 23:17:20 +0300

    read data from file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+""" this module contains one function """
+
+
+def read_file(filename=""):
+    """function to read a file"""
+    with open(filename, encoding="utf-8") as my_file:
+        data = my_file.read()
+        print(data)
