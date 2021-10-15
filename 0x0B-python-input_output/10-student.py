f86c60a3dc5d5ddb6208a0671f11ff2e490c8438
mosesmrima <mrimamss@gmail.com>
2021-10-15 10:55:25 +0300

    check if self has attr

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/pyton3
+""" this module contains one class """
+
+
+class Student:
+    """student class defination"""
+
+    def __init__(self, first_name, last_name, age):
+        """init method"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """return dict repr of self"""
+        if type(attrs) is list and all(type(s) is str for s in attrs):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
