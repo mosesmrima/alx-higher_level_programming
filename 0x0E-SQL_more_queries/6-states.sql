f13f37e0c8e0a9015751788e76acdcf69ad6061e
mosesmrima <mrimamss@gmail.com>
2021-11-18 00:55:23 +0300

    table with column constarains

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,4 @@
+-- create table with consyataints
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (id INT UNIQUE AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL);
