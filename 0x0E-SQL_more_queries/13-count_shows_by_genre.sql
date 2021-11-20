e6bcc545d5a9a86c6212321d3482c52310116efc
mosesmrima <mrimamss@gmail.com>
2021-11-20 23:06:41 +0300

    count number of shows

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- count number of shows in each genre
+
+SELECT
+	g.name AS genre, COUNT(*)  AS number_of_shows
+	FROM tv_genres AS g
+	INNER JOIN tv_show_genres AS s
+	ON g.id = s.genre_id
+	GROUP BY g.name
+	ORDER BY number_of_shows DESC;
