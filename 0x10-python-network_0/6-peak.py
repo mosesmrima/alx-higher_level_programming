a0d77b9f0b3b6c233ca24fd0f636c6eee58f6032
mosesmrima <mrimamss@gmail.com>
2022-01-25 00:55:30 +0300

    fix linter erors

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,16 +7,16 @@ def find_peak(list_of_integers):
     if list_of_integers == []:
         return None
 
-    s    = len(list_of_integers)
-    if s    == 1:
+    s = len(list_of_integers)
+    if s == 1:
         return list_of_integers[0]
-    elif s    == 2:
+    elif s == 2:
         return max(list_of_integers)
 
-    mid = int(s  2)
-    p    = list_of_integers[mid]
-    if p > ist_of_integers[mid - 1] and p > list_of_integers[mid + 1]:
-        return peak
+    mid = int(s / 2)
+    p = list_of_integers[mid]
+    if p > list_of_integers[mid - 1] and p > list_of_integers[mid + 1]:
+        return p
     elif p < list_of_integers[mid - 1]:
         return find_peak(list_of_integers[:mid])
     else:
