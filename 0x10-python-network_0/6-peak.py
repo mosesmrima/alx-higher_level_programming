e134d910a7eb5cc9f9eb962089113655a5acfbae
mosesmrima <mrimamss@gmail.com>
2022-01-25 00:50:43 +0300

    fid peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""find peak of an array"""
+
+
+def find_peak(list_of_integers):
+    """Return a peak in a list of unsorted integers."""
+    if list_of_integers == []:
+        return None
+
+    s    = len(list_of_integers)
+    if s    == 1:
+        return list_of_integers[0]
+    elif s    == 2:
+        return max(list_of_integers)
+
+    mid = int(s  2)
+    p    = list_of_integers[mid]
+    if p > ist_of_integers[mid - 1] and p > list_of_integers[mid + 1]:
+        return peak
+    elif p < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
