0c6f252ca0671c2910f6e2371116bc5c292be45e
mosesmrima <mrimamss@gmail.com>
2022-01-26 14:24:10 +0300

    fix typo in import

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,7 +9,7 @@ import urllib.parse
 import sys
 
 if __name__ == "__main__":
-    url = sys.arv[1]
+    url = sys.argv[1]
     email = {"email": sys.argv[2]}
     data = urllib.parse.urlencode(email).encode("ascii")
     req = urllib.request.Request(url, data)
