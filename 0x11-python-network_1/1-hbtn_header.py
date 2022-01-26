b1390760021c71c0f684a172cffcd817b3a55f47
mosesmrima <mrimamss@gmail.com>
2022-01-26 13:34:06 +0300

    fetch headers from a response

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""script that takes in a URL, sends a request to the URL and
+ displays the value of the X-Request-Id variable found in the
+ header of the response. """
+
+import sys
+import urllib.request
+
+url = sys.argv[1]
+req = urllib.request.Request(url)
+with urllib.request.urlopen(req) as res:
+    print(res.headers["X-Request-Id"])
