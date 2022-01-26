13ca6a97164a47a2e59c0704016a7ef4c65362b8
mosesmrima <mrimamss@gmail.com>
2022-01-26 13:42:41 +0300

    prevent execution on import

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,11 +2,11 @@
 """ cript that fetches https://intranet.hbtn.io/status """
 
 import urllib.request
-
-req = urllib.request.Request("https://intranet.hbtn.io/status")
-with urllib.request.urlopen(req) as res:
-    body = res.read()
-    print("Body response:")
-    print("\t - type: {}".format(type(body)))
-    print("\t - content: {}".format(body))
-    print("\t - utf8 content: {}".format(body.decode("utf-8")))
+if __name__ == "__main__":
+    req = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(req) as res:
+        body = res.read()
+        print("Body response:")
+        print("\t - type: {}".format(type(body)))
+        print("\t - content: {}".format(body))
+        print("\t - utf8 content: {}".format(body.decode("utf-8")))
