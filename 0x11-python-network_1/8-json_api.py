f8b1b7144063f84f27c8d3c9506f2f347dd69218
mosesmrima <mrimamss@gmail.com>
2022-01-27 17:55:03 +0300

    convert to json inside try exc

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
     letter = "" if (len(sys.argv) == 1) else sys.argv[1]
     payload = {"q": letter}
     r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
-    response = r.json()
     try:
+        response = r.json()
         if response == {}:
             print("No result")
         else:
