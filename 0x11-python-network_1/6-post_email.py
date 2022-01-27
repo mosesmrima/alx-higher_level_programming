29a83a3b7c869c609e0970b9e7063391aff75820
mosesmrima <mrimamss@gmail.com>
2022-01-27 17:16:53 +0300

    fix cmd args order

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,7 +7,7 @@ import sys
 import requests
 
 if __name__ == "__main__":
-    url = sys.argv[2]
+    url = sys.argv[1]
     payload = {"email": sys.argv[2]}
     r = requests.post(url, data=payload)
     print(r.text)
