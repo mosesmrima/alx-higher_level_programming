bbb3fd81eab9d231cbcbbafe9420585f1d73f2cf
mosesmrima <mrimamss@gmail.com>
2022-01-27 19:25:38 +0300

    handle for index errors

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,10 +11,13 @@ if __name__ == "__main__":
         sys.argv[1], sys.argv[2])
     r = requests.get(url)
     commits = r.json()
-    for i in range(10):
-        print(
-            "{}: {}".format(
-                commits[i].get("sha"),
-                commits[i].get("commit").get("author").get("name"),
+    try:
+        for i in range(10):
+            print(
+                "{}: {}".format(
+                    commits[i].get("sha"),
+                    commits[i].get("commit").get("author").get("name"),
+                )
             )
-        )
+    except IndexError:
+        pass
