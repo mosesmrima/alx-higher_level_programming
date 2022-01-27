feb8f1fee05e7f1363be6c44c7526e7edcb41053
mosesmrima <mrimamss@gmail.com>
2022-01-27 19:28:46 +0300

    catch index and key errors

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -19,5 +19,5 @@ if __name__ == "__main__":
                     commits[i].get("commit").get("author").get("name"),
                 )
             )
-    except IndexError:
+    except (KeyError, IndexError):
         pass
