466301ffb274df20b8492ed81b4fddff11367c91
mosesmrima <mrimamss@gmail.com>
2022-01-27 19:11:39 +0300

    get 10 latest commits from a repo

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" get 10 latest comments """
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".
+    format(sys.argv[1], sys.argv[2])
+    r = requests.get(url)
+    commits = r.json()
+    for i in range(10):
+        print(
+            "{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name"),
+            )
+        )
