1cfbc26b85d458656b993260345a6023fd8f655a
mosesmrima <mrimamss@gmail.com>
2022-01-28 11:57:02 +0300

    find number of occurences

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let i = 0;
+  let count = 0;
+  for (; i < list.length; i++) {
+    if (list[i] === searchElement) count += 1;
+  }
+  return count;
+};
