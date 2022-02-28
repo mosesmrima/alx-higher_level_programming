962730b6e332b4916240b66cc5ca7271a4aee3ea
mosesmrima <mrimamss@gmail.com>
2022-02-28 20:07:38 +0300

    return count

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,7 +7,7 @@ request(url, (err, res, body) => {
   if (!err) {
     const movies = JSON.parse(body).results;
     console.log(movies.reduce((count, movie) => {
-      return movie.characters.find(cha => cha.endsWith('/18/')) ? count + 1 : count;
+	return movie.characters.find((cha) => cha.endsWith('/18/')) ? count + 1 : count;
     }, 0));
   }
 });
