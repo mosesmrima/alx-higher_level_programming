6cb3170a8bb5be91271e5a5da40ac2e01ba2d555
mosesmrima <mrimamss@gmail.com>
2022-02-28 19:54:44 +0300

    pass on error

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,9 +4,7 @@ const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/';
 
 request(url, (err, res, body) => {
-  if (err) {
-    console.log(err);
-  } else {
+  if (!err) {
     const movies = JSON.parse(body).results;
     console.log(movies.reduce((count, movie) => {
       return movie.characters.find(cha => cha.endsWith('/18/')) ? count + 1 : count;
