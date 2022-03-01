ace878958cf41c836b5461abfa07bc9e397ed471
mosesmrima <mrimamss@gmail.com>
2022-03-01 07:32:13 +0300

    print in synchronous mode

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,18 +1,21 @@
 #!/usr/bin/node
-
 const request = require('request');
 const id = process.argv[2];
 const url = `https://swapi-api.hbtn.io/api/films/${id}/`;
-
-request(url, (err, res, body) => {
-  if (!err) {
+request(url, function (error, response, body) {
+  if (!error) {
     const characters = JSON.parse(body).characters;
-    for (let i = 0; i < characters.length; i++) {
-      request(characters[i], (err1, res1, body1) => {
-        if (!err1) {
-          console.log(JSON.parse(body1).name);
-        }
-      });
-    }
+    printCharacters(characters, 0);
   }
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
