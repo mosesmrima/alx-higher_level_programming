44d95f3a46892ab692d6303854a4e765227175ab
mosesmrima <mrimamss@gmail.com>
2022-03-01 05:22:49 +0300

    use url from process.argv

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/';
+const url = process.argv[2];
 
 request(url, (err, res, body) => {
   if (!err) {
