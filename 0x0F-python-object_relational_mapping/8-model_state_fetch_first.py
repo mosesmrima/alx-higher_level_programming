86a0e9d6cb5b9045ef51f0d08446d30793fbd8d9
mosesmrima <mrimamss@gmail.com>
2022-04-03 13:44:17 +0300

    handle for empty set

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -22,5 +22,7 @@ if __name__ == "__main__":
     session = Session()
 
     state = session.query(State).first()
-
-    print("{}: {}".format(state.id, state.name))
+    if state is not None:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("Nothing")
