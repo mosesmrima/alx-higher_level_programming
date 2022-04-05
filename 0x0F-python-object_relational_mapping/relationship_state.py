05400227c39f738bd381bc16fffa331d3a378684
mosesmrima <mrimamss@gmail.com>
2022-04-05 11:28:56 +0300

    fix pep8 error

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -5,7 +5,7 @@ this module comtains the state class
 """
 
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column,Integer, ForeignKey, String
+from sqlalchemy import Column, Integer, ForeignKey, String
 from relationship_city import Base
 
 
@@ -13,6 +13,7 @@ class State(Base):
     """
     represents the states table
     """
+
     __tablename__ = "states"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
